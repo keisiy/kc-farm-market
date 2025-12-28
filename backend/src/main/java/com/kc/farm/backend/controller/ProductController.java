256c18170c37307049b6a406237a96348b337f42
弓田敬史 <yumitakeisi@Mac.lan>
2025-12-28 12:08:14 +0900

    Get １件取得完了

--- backend/src/main/java/com/kc/farm/backend/controller/ProductController.java
+++ backend/src/main/java/com/kc/farm/backend/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.kc.farm.backend.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,12 @@ public class ProductController {
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
+	
+	@GetMapping("/{id}")
+	public Product getById(@PathVariable Long id) {
+		return productRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Product not found"));
+	}
 	
 	/* 商品登録 */
 	@PostMapping
