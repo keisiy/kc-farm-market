d6986e844154f2865d2ba54f97ebd0cebe861375
弓田敬史 <yumitakeisi@yudakeishinoMacBook-Pro.local>
2026-01-03 16:04:56 +0900

    fetch理解着手

--- backend/src/main/java/com/kc/farm/backend/controller/ProductController.java
+++ backend/src/main/java/com/kc/farm/backend/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.validation.Valid;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.kc.farm.backend.service.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin(origins = "*")// 学習用・本番では使用しない！
 public class ProductController {
 	
 	private final ProductService productService;
