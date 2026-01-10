9d98b2dbd8a0a5e5ae670f9a8343ef82178b9fe0
弓田敬史 <yumitakeisi@yudakeishinoMacBook-Pro.local>
2026-01-10 17:39:09 +0900

    CORS設定ファイル作成完了

--- backend/src/main/java/com/kc/farm/backend/controller/ProductController.java
+++ backend/src/main/java/com/kc/farm/backend/controller/ProductController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import jakarta.validation.Valid;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,6 @@ import com.kc.farm.backend.service.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
 	
 	private final ProductService productService;
